0b366068a02f8200f75c26bb94eae8523029bfac
imjeongjun <jjlim1004@naver.com>
2021-10-23 16:57:56 +0900

    commit

---
+++ src/main/java/com/example/demomo/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.example.demomo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/test")
+    public void test(){}
+}
